f1ce07e304c2dd2075ebe57f31618ab5975bb70a
KnoxNeon <iftey10@gmail.com>
2025-09-09 17:38:37 +0600

    add total price

--- script/index.js
+++ script/index.js
@@ -32,13 +32,15 @@ const loadAllTrees = async() => {
         clickedAllbtn.classList.add("active")
         displayTrees(data.plants)
     }
-    
+
+let totalPrice = 0;
+
 const addToCart = async (id) =>{
     const url = `https://openapi.programming-hero.com/api/plant/${id}`
     const res = await fetch(url)
     const data = await res.json()
 
-    let totalPrice = 0;
+    totalPrice = totalPrice + data.plants.price;
 
     const allPrice = document.getElementById("cart-container")
 
@@ -48,16 +50,25 @@ const addToCart = async (id) =>{
                 <div class="addToCart bg-[#F0FDF4] rounded-lg mx-2 py-2 px-4 flex justify-between items-center">
                   <div class="flex flex-col gap-2 font-semibold text-base text-left">
                   <h3>${data.plants.name}</h3>
-                  <p class="text-[#577094] text-sm font-normal">৳${data.plants.price} × 1</p>
+                  <p class="text-[#577094] text-sm font-normal">৳ ${data.plants.price} × 1</p>
                   </div>
-                  <i class="fa-solid fa-xmark"></i>
+                  <i class="fa-solid fa-xmark cursor-pointer"></i>
                 </div>
         `;
+
         allPrice.append(priceDiv);
+
+        const xPrice = priceDiv.querySelector(".fa-xmark");
+
+        xPrice.addEventListener("click", ()=>{
+            priceDiv.remove()
+            totalPrice = totalPrice - data.plants.price;
+            document.getElementById("total-price").innerText = `৳${totalPrice}`
+
+        })
+
+        document.getElementById("total-price").innerText = `৳${totalPrice}`
 }
-
-
-    
 
 
 const displayTrees = (trees) => {
@@ -76,7 +87,7 @@ const displayTrees = (trees) => {
                     <h3 class="text-sm bg-[#DCFCE7] rounded-xl p-1 text-[#15803D]">${tree.category}</h3>
                     <h3 class="font-semibold">৳${tree.price}</h3>
                    </div>  
-                   <button onclick="addToCart(${tree.id})" class="bg-[#15803D] text-white text-base font-medium w-full rounded-3xl py-1">Add to Cart</button>
+                   <button onclick="addToCart(${tree.id})" class="bg-[#15803D] text-white text-base font-medium w-full rounded-3xl py-1 cursor-pointer">Add to Cart</button>
                    </div>
         `;
         allTree.append(treeDiv);
@@ -90,7 +101,7 @@ const displayCategory = (categories) => {
 
     const allBtn = document.createElement("div");
     allBtn.innerHTML = `
-         <button id="category-all"  onclick="loadAllTrees()" class="bg-white active w-full grid justify-center md:justify-start md:ml-5 p-1 text-left font-medium text-base rounded-md category-btn">All Plants</button>
+         <button id="category-all"  onclick="loadAllTrees()" class="bg-white active w-full grid justify-center md:justify-start md:ml-5 p-1 text-left font-medium text-base rounded-md category-btn cursor-pointer">All Plants</button>
     `;
     categoryContainer.append(allBtn);
 
@@ -99,7 +110,7 @@ const displayCategory = (categories) => {
         
         catDiv.innerHTML = `
         
-        <button id="category-${category.id}" onclick="loadTrees(${category.id})" class="bg-white  w-full grid justify-center md:justify-start md:ml-5 p-1 text-left font-medium text-base rounded-md category-btn">${category.category_name}s</button>
+        <button id="category-${category.id}" onclick="loadTrees(${category.id})" class="bg-white  w-full grid justify-center md:justify-start md:ml-5 p-1 text-left font-medium text-base rounded-md category-btn cursor-pointer">${category.category_name}s</button>
         `;
         categoryContainer.append(catDiv);
     }
